9f66b2afe871b1d52575d4f1aa61884279e71f1e
Jack <pupeljack@gmail.com>
2020-01-28 14:45:53 -0600

    found 3rd biggest

--- drills.js
+++ drills.js
@@ -11,7 +11,7 @@ class BinarySearchTree {
       this.key = key;
       this.value = value;
     } else if (key < this.key) {
-      if(this.left === null) {
+      if (this.left === null) {
         this.left = new BinarySearchTree(key, value, this)
       } else {
         this.left.insert(key, value)
@@ -24,28 +24,28 @@ class BinarySearchTree {
       }
     }
   }
-  remove (key) {
-    if(this.key === key) {
-      if(this.left && this.right) {
+  remove(key) {
+    if (this.key === key) {
+      if (this.left && this.right) {
         const successor = this.right._findMin();
         this.key = successor.key;
         this.value = successor.value;
         successor.remove(successor.key);
       } else if (this.left) {
-        this._replaceWith(this.left) 
+        this._replaceWith(this.left)
       } else if (this.right) {
         this._replaceWith(this.right)
       } else {
         this._replaceWith(null)
       }
     } else {
-      throw new Error ('Key Error')
+      throw new Error('Key Error')
     }
   }
-  find (key) {
+  find(key) {
     if (this.key === key) {
       return this.value;
-    } else if(key < this.key && this.left) {
+    } else if (key < this.key && this.left) {
       return this.left.find(key)
     } else {
       throw new Error('No key found')
@@ -53,53 +53,62 @@ class BinarySearchTree {
   }
 }
 
-function main () {
+function main() {
   let BST = new BinarySearchTree();
-  // BST.insert(3,3)
-  // BST.insert(1,1)
-  // BST.insert(4,4)
-  // BST.insert(6,6)
-  // BST.insert(9,9)
-  // BST.insert(2,2)
-  // BST.insert(5,5)
-  // BST.insert(7,7)
+  BST.insert(3, 3)
+  BST.insert(1, 1)
+  BST.insert(4, 4)
+  BST.insert(6, 6)
+  BST.insert(9, 9)
+  BST.insert(2, 2)
+  BST.insert(5, 5)
+  BST.insert(7, 7)
 
-  function tree(t){
-    if(!t){
-        return ' ';
-    }
-    return tree(t.left) + t.value + tree(t.right)
-}
 
-  BST.insert('E', 'E')
+
+  /*BST.insert('E', 'E')
   BST.insert('A', 'A')
   BST.insert('S', 'S')
-  BST.insert('Y','Y')
-  BST.insert('Q','Q')
-  BST.insert('U','U')
-  BST.insert('T','T')
-  BST.insert('I','I')
-  BST.insert('O','O')
-  BST.insert('N','N') 
-  //console.log(BST.right.left);
-  //console.log(BST.right.right);
-  console.log(tree(BST));
-  console.log(findHeight(BST));
+  BST.insert('Y', 'Y')
+  BST.insert('Q', 'Q')
+  BST.insert('U', 'U')
+  BST.insert('T', 'T')
+  BST.insert('I', 'I')
+  BST.insert('O', 'O')
+  BST.insert('N', 'N')*/
+ //console.log(BST);
+  //console.log('3rd big is ', find3rdBiggest(BST));
+  //console.log('third big is ', findThirdBiggest(BST));
+  //console.log(tree(BST));
+  //console.log(findHeight(BST));
 }
 
 main()
 
-function findHeight(tree){
-  if(!tree){
+function tree(t) {
+  if (!t) {
+    return '';
+  }
+  return tree(t.left) + t.value + tree(t.right)
+}
+
+function findHeight(tree) {
+  if (!tree) {
     return 0;
   }
 
   const heightLeft = 1 + findHeight(tree.left);
   const heightRight = 1 + findHeight(tree.right);
 
-  if(heightLeft > heightRight){
+  if (heightLeft > heightRight) {
     return heightLeft;
   }
   return heightRight;
+}
+
+
+function find3rdBiggest(input){
+  const res = tree(input).split('');
+  return res[res.length -3];
 }
 
